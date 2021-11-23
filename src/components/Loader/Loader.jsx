e966711c9d93e1b905a953f70a9192428f001cb8
OR_GOLESTANI <orgolestani@gmail.com>
2021-11-23 03:35:42 +0200

    gh-pages update

--- src/components/Loader/Loader.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './Loader.scss'
-export default function Loader() {
-    return (
-       <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-    )
-}
